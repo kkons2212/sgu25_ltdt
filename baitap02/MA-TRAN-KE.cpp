6603a28d7060e3fbd719cb808aa1ca59276afdad
Quocbao <thaidangquocbao@gmail.com>
2025-09-21 02:54:32 +0000

    ma tran ke

---
+++ baitap02/MA-TRAN-KE.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+#define FI "MA-TRAN-KE.INP"
+#define FO " MA-TRAN-KE.OUT"
+using namespace std;
+int n;
+vector<vector<int>> a;
+vector<int>bac;
+void nhap()
+{
+    cin>>n;
+    a.resize(n);
+    for(int i=0;i<n;i++)
+    {
+        a[i].resize(n);
+        for(int j=0;j<n;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    bac.resize(n);
+    for(int i=0;i<n;i++)
+    {
+        int baci=0; // tinh bac cua dinh thu i
+        for(int j=0;j<n;j++)
+        {
+            baci=baci+a[i][j];
+        }
+        bac[i]=baci;
+    }
+
+}
+void xuat()
+{
+    cout<<n<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<bac[i]<<endl;
+    }
+}
+int main()
+{
+    freopen(FI,"r",stdin);
+    freopen(FO,"w",stdout);
+    nhap();
+    xuat();
+    return 0;
+}
